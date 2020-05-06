703128dae2e9c947abb5c15f9b076d6dfea58ffc
Dealead <64843859+Dealead@users.noreply.github.com>
2020-05-06 01:09:58 +0100

    Create HashA.py

---
+++ .github/workflows/HashA.py
@@ -0,0 +1,75 @@
+import pandas as pd
+
+
+
+
+assignment_sheet1 = pd.read_excel("ADETUNJI DANIEL - ASSIGNMENT 2.xlsx", sheet_name="JANUARY")
+print(assignment_sheet1)
+
+assignment_sheet2 = pd.read_excel("ADETUNJI DANIEL - ASSIGNMENT 2.xlsx", sheet_name="FEBRUARY")
+print(assignment_sheet2)
+
+assignment_sheet3 = pd.read_excel("ADETUNJI DANIEL - ASSIGNMENT 2.xlsx", sheet_name="MARCH")
+print(assignment_sheet3)
+
+assignment_sheet4 = pd.read_excel("ADETUNJI DANIEL - ASSIGNMENT 2.xlsx", sheet_name="APRIL")
+print(assignment_sheet4)
+
+assignment_sheet5 = pd.read_excel("ADETUNJI DANIEL - ASSIGNMENT 2.xlsx", sheet_name="MAY")
+print(assignment_sheet5)
+
+assignment_sheet6 = pd.read_excel("ADETUNJI DANIEL - ASSIGNMENT 2.xlsx", sheet_name="JUNE")
+print(assignment_sheet6)
+
+assignment_sheet7 = pd.read_excel("ADETUNJI DANIEL - ASSIGNMENT 2.xlsx", sheet_name="JULY")
+print(assignment_sheet7)
+
+assignment_sheet8 = pd.read_excel("ADETUNJI DANIEL - ASSIGNMENT 2.xlsx", sheet_name="AUGUST")
+print(assignment_sheet8)
+
+assignment_sheet9 = pd.read_excel("ADETUNJI DANIEL - ASSIGNMENT 2.xlsx", sheet_name="SEPTEMBER")
+print(assignment_sheet9)
+
+assignment_sheet10 = pd.read_excel("ADETUNJI DANIEL - ASSIGNMENT 2.xlsx", sheet_name="JANUARY")
+print(assignment_sheet10)
+
+
+
+
+
+
+
+assignment_sheet1.to_csv("ADETUNJI DANIEL-ASSIGNMENT 2 - SHEET 1 (JANUARY).csv")
+print(assignment_sheet1)
+
+assignment_sheet2.to_csv("ADETUNJI DANIEL-ASSIGNMENT 2 - SHEET 2 (FEBURARY).csv")
+print(assignment_sheet2)
+
+assignment_sheet3.to_csv("ADETUNJI DANIEL-ASSIGNMENT 2 - SHEET 3 (MARCH).csv")
+print(assignment_sheet3)
+
+assignment_sheet4.to_csv("ADETUNJI DANIEL-ASSIGNMENT 2 - SHEET 4 (APRIL).csv")
+print(assignment_sheet4)
+
+assignment_sheet5.to_csv("ADETUNJI DANIEL-ASSIGNMENT 2 - SHEET 5 (MAY).csv")
+print(assignment_sheet5)
+
+assignment_sheet6.to_csv("ADETUNJI DANIEL-ASSIGNMENT 2 - SHEET 6 (JUNE).csv")
+print(assignment_sheet6)
+
+assignment_sheet7.to_csv("ADETUNJI DANIEL-ASSIGNMENT 2 - SHEET 7 (JULY).csv")
+print(assignment_sheet7)
+
+assignment_sheet8.to_csv("ADETUNJI DANIEL-ASSIGNMENT 2 - SHEET 8 (AUGUST).csv")
+print(assignment_sheet8)
+
+assignment_sheet9.to_csv("ADETUNJI DANIEL-ASSIGNMENT 2 - SHEET 9 (SEPTEMBER).csv")
+print(assignment_sheet9)
+
+assignment_sheet10.to_csv("ADETUNJI DANIEL-ASSIGNMENT 2 - SHEET 10 (OCTOBER).csv")
+print(assignment_sheet10)
+
+
+
+
+
